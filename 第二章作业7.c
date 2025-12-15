1e807ed8c3e5f946cf9619634fab54102c4f583b
Yoisakiyuki <yukiraku871116@126.com>
2025-12-15 23:12:19 +0800

    Add files via upload

---
+++ 第二章作业7.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(void)
+{
+	int a;
+	int b;
+	a = 3020;/*a为words的值*/
+	b = 350; /*b为lines的值*/
+	printf("There were %d words and %d lines", a, b);
+}
---
+++ 调试程序.c
@@ -0,0 +1,15 @@
+/* nogood.c -- ÓÐ´íÎóµÄ³ÌÐò */
+#include <stdio.h>
+int main(void)
+{
+
+    int n;
+    int n2;
+    int n3;
+    n = 5;
+    n2 = n * n;
+    n3 = n * n * n;
+    printf("n = %d, n squared = %d, n cubed = %d\n", n, n2, n3);
+
+        return 0;
+}
